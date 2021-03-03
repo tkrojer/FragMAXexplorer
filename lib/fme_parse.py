2faf75324780dbcae790be41c683722c98bc0c15
Tobias Krojer <tobias.krojer@sgc.ox.ac.uk>
2021-03-03 22:52:13 +0100

    first commit

--- lib/fme_parse.py
+++ lib/fme_parse.py
@@ -393,6 +393,7 @@ class read_dimple(QtCore.QThread):
                 db_dict['RefinementMTZ_latest'] = os.path.join(s,'dimple','final.mtz')
                 pdbDict = fme_xtaltools.pdbtools(os.path.join(s,'dimple','final.pdb')).get_refinement_stats_dict()
                 runs = self.db.runs_of_crystal_in_plexTable(x)
+                print('runs', runs)
                 for r in runs:
                     db_dict['DataCollectionRun'] = r
                     db_dict['DataProcessingProgram'] = 'autoproc'
